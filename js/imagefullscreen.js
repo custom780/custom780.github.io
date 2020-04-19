188054de335853c7c81bf8e4bb39d684026fe375
custom780 <59577986+custom780@users.noreply.github.com>
2020-04-19 12:49:17 +0300

    Update imagefullscreen.js

--- js/imagefullscreen.js
+++ js/imagefullscreen.js
@@ -1,40 +1,56 @@
-
 function makeFullScreen() {
-  if (
-    document.fullscreenEnabled || 
-    document.webkitFullscreenEnabled || 
-    document.mozFullScreenEnabled ||
-    document.msFullscreenEnabled
-  ) {
-    var i = document.getElementById("myImage");
-    i.onclick = function() {
-      if (
-        document.fullscreenElement ||
-        document.webkitFullscreenElement ||
-        document.mozFullScreenElement ||
-        document.msFullscreenElement
-      ) {
-        if (document.exitFullscreen) {
-          document.exitFullscreen();
-        } else if (document.webkitExitFullscreen) {
-          document.webkitExitFullscreen();
-        } else if (document.mozCancelFullScreen) {
-          document.mozCancelFullScreen();
-        } else if (document.msExitFullscreen) {
-          document.msExitFullscreen();
-        }
-      }
-      else {
-        if (i.requestFullscreen) {
-          this.requestFullscreen();
-        } else if (i.webkitRequestFullscreen) {
-          i.webkitRequestFullscreen();
-        } else if (i.mozRequestFullScreen) {
-          i.mozRequestFullScreen();
-        } else if (i.msRequestFullscreen) {
-          i.msRequestFullscreen();
-        }
-      }
-    }
-  }
+
+
+	// full-screen available?
+	if (
+		document.fullscreenEnabled || 
+		document.webkitFullscreenEnabled || 
+		document.mozFullScreenEnabled ||
+		document.msFullscreenEnabled
+	) {
+
+		// image container
+		var i = document.getElementById("myImage2");
+
+		// click event handler
+		i.onclick = function() {
+
+			// in full-screen?
+			if (
+				document.fullscreenElement ||
+				document.webkitFullscreenElement ||
+				document.mozFullScreenElement ||
+				document.msFullscreenElement
+			) {
+
+				// exit full-screen
+				if (document.exitFullscreen) {
+					document.exitFullscreen();
+				} else if (document.webkitExitFullscreen) {
+					document.webkitExitFullscreen();
+				} else if (document.mozCancelFullScreen) {
+					document.mozCancelFullScreen();
+				} else if (document.msExitFullscreen) {
+					document.msExitFullscreen();
+				}
+
+			}
+			else {
+
+				// go full-screen
+				if (i.requestFullscreen) {
+					this.requestFullscreen();
+				} else if (i.webkitRequestFullscreen) {
+					i.webkitRequestFullscreen();
+				} else if (i.mozRequestFullScreen) {
+					i.mozRequestFullScreen();
+				} else if (i.msRequestFullscreen) {
+					i.msRequestFullscreen();
+				}
+
+			}
+
+		}
+
+	}
 }
